9e8f21a8ac09d123075086a78eb3560f06f161f6
Phillip Cloud <cloud@standard.ai>
2020-05-07 08:19:56 -0400

    Cleanup nix shell and allow different rust channels

--- shell.nix
+++ shell.nix
@@ -2,19 +2,30 @@ let
   moz_overlay = import (
     builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz
   );
-  nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
-  stable = nixpkgs.rustChannelOf { channel = "stable"; };
+  pkgs = import <nixpkgs> {
+    overlays = [ moz_overlay ];
+  };
   extensions = [
-      "clippy-preview"
-      "rls-preview"
-      "rustfmt-preview"
-      "rust-analysis"
-      "rust-std"
-      "rust-src"
+    "clippy-preview"
+    "rls-preview"
+    "rustfmt-preview"
+    "rust-analysis"
+    "rust-std"
+    "rust-src"
   ];
-  rust-stable = stable.rust.override { extensions = extensions; };
-in with nixpkgs;
-  mkShell {
-    name = "rust-stable";
-    buildInputs = [ rust-stable ];
-  }
+  mkRustShell = channel: {
+    ${channel} = pkgs.mkShell {
+      name = "rust-${channel}-shell";
+      buildInputs = [
+        ((pkgs.rustChannelOf { inherit channel; }).rust.override { inherit extensions; })
+        pkgs.cargo-audit
+      ];
+    };
+  };
+  channels = [
+    "stable"
+    "beta"
+    "nightly"
+  ];
+in
+pkgs.lib.fold (left: right: left // right) {} (builtins.map mkRustShell channels)
